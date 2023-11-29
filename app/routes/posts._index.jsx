660abeeb6375a48365a30302c3c8cf07418e61af
MoMoZin <khinmomozin@gmail.com>
2023-11-29 16:26:33 -0700

    form submission

--- app/routes/posts._index.jsx
+++ app/routes/posts._index.jsx
@@ -1,7 +1,37 @@
+import { Link, useLoaderData } from "@remix-run/react";
+
+export const loader = () => {
+  //server side
+
+  const data = {
+    posts: [
+      {id: 1, title: "Post 1", body: "This is a test post"},
+      {id: 2, title: "Post 2", body: "This is a test post"},
+      {id: 3, title: "Post 3", body: "This is a test post"},
+      {id: 4, title: "Post 4", body: "This is a test post"},
+    ]
+  }
+  return data;
+}
+
 function PostItems() {
+  const { posts } = useLoaderData();
+
   return (
     <div>
-      <h1>Posts</h1>
+      <div className="page-header">        
+        <h1>Posts</h1>
+        <Link to='/posts/new' className="btn"> New Post </Link>
+      </div>
+      <ul className="posts-list">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={`/posts/${post.id}`}>
+              <h3>{post.title}</h3>
+            </Link>
+          </li>
+        ))}
+        </ul>
     </div>
   )
 }
--- app/routes/posts.new.jsx
+++ app/routes/posts.new.jsx
@@ -1,9 +1,41 @@
-function newPost() {
+import { redirect } from "@remix-run/node";
+import { Form, Link } from "@remix-run/react";
+export const action = async ({ request }) => {
+  const form = await request.formData();
+  const title = form.get("title");
+  const body = form.get("body");
+
+  const fields = { title, body };
+
+  //@todo - submit to database
+
+  console.log(fields)
+  return redirect('/posts');
+}
+
+function NewPost() {  
   return (
-    <div>
-      <h1>New Post</h1>
-    </div>
+    <>
+      <div className="page-header">
+        <h1>New Post</h1>
+        <Link to="/posts" className="btn btn-reverse">Back</Link>
+      </div>
+
+      <div className="page-content">
+        <Form method='POST'>
+          <div className="form-control">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" id="title" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="body">Post Body</label>
+            <textarea name="body" id="body" />
+            <button type="submit" className="btn btn-block">Add Post</button>
+          </div>
+        </Form>
+      </div>
+    </>
   )
 }
 
-export default newPost
+export default NewPost
